3d8f56004e96b9613bba25301ed8858c23a81be4
sunil saini <sunilsaini67193@gmail.com>
2025-07-25 14:31:16 +0530

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Login_Page.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login_Page.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
